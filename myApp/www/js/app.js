31412de78739e289736b38ef30d4a3a201eae979
Iven <ivenxu@gmail.com>
2015-07-13 21:36:16 +0800

    adjust layout

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -80,6 +80,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $ionicConfigProvider.tabs.position('bottom');
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/chats');
 
 });
